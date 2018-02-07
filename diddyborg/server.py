6e028a9f2759344179be3d7cb24d36bd296e45ad
cjwebb <colinjameswebb@hotmail.com>
2018-02-07 07:12:11 +0000

    Mux h264 video to mp4 on the fly with ffmpeg
    It currently jutters a lot, presumably because the frame-rate is
    unknown... although it might just be that this solution is too slow.

--- diddyborg/server.py
+++ diddyborg/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, Response, render_template
+import subprocess
 
 app = Flask(__name__)
 
@@ -6,12 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template('remotecontrol.html')
 
-f = open('video.mp4', 'rb')
+# ffmpeg used inside of docker container
+#   -c:v h264 -i - says that we expect h264 from stdin, denoted with '-i -'.
+#   -movflags option is specified on the output so that mp4 can be streamed. it fails to encode otherwise.
+#   -f mp4 - specifies that we write an mp4 to stdout.
+cmd = 'docker run -i --rm jrottenberg/ffmpeg:3.4-alpine -c:v h264 -i - -movflags frag_keyframe+empty_moov -f mp4 -'
+p = subprocess.Popen('cat video.h264 | ' + cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
 @app.route('/video.mp4')
 def video_stream():
     def generate():
-        for data in iter(f):
+        for data in iter(p.stdout.readline, b''):
             yield data
     return Response(generate(), mimetype='video/mp4')
 
