68d29b751663f005267384b210859cc0a2409931
cjwebb <colinjameswebb@hotmail.com>
2018-02-08 08:06:58 +0000

    update ffmpeg command

--- diddyborg/server.py
+++ diddyborg/server.py
@@ -7,11 +7,12 @@ app = Flask(__name__)
 def index():
     return render_template('remotecontrol.html')
 
-# ffmpeg used inside of docker container
-#   -c:v h264 -i - says that we expect h264 from stdin, denoted with '-i -'.
-#   -movflags option is specified on the output so that mp4 can be streamed. it fails to encode otherwise.
-#   -f mp4 - specifies that we write an mp4 to stdout.
-cmd = 'docker run -i --rm jrottenberg/ffmpeg:3.4-alpine -c:v h264 -i - -movflags frag_keyframe+empty_moov -f mp4 -'
+cmd = 'docker run -i --rm jrottenberg/ffmpeg:3.4-alpine' \
+        ' -c:v h264' \ # input is h264
+        ' -i -' \ # input from stdin
+        ' -movflags frag_keyframe+empty_moov' \ # allows mp4 to be streamable
+        ' -vf scale=640:-1' \ # scales output to 640px in height, and a width to keep the current ratio.
+        ' -f mp4 -' # output in mp4 to stdout
 p = subprocess.Popen('cat video.h264 | ' + cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
 @app.route('/video.mp4')
